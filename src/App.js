82eadec97c97c157fd222bc94bdfed987c97444b
PrathicSundararajan <prathic44@gmail.com>
2020-11-15 12:55:07 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 
+//Edit <code>src/App.js</code> and save to reload.
 function App() {
-  return (
+  return ( 
+    //Edit <code>src/App.js</code> and save to reload.
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo" />        
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        The beginning of the creation of my new portfolio using ReactJS!  
         </p>
         <a
           className="App-link"
